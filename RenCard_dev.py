8f66291ea8302feb736d6173601b9a2851461a95
kevinlee963 <kevinlee963@users.noreply.github.com>
2013-11-14 13:53:49 -0800

    Update RenCard_dev.py

--- RenCard_dev.py
+++ RenCard_dev.py
@@ -1,7 +1,9 @@
 #-------------------------------------------------------------------------------
 # Name:        RenCard
-# Purpose:     Renaissance Card Sorter and Counter#
-# Author:      leekevin#
+# Purpose:     Renaissance Card Counter and Sorter
+#
+# Author:      leekevin
+#
 # Created:     04/11/2013
 # Copyright:   (c) leekevin 2013
 # Licence:     Creative Commons: Attribution-NonCommercial-ShareAlike 3.0 Unported
@@ -11,10 +13,11 @@ import sys
 
 def gpa():
     home.destroy()
-    gpa_win = Frame(win,bg='white')
+    gpa_win = Frame(win,bg='white',bd = 0)
     gpa_win.pack()
     welcome = Label(gpa_win, text ='GPA Mode',font = 'Arial 16')
     welcome.grid(row = 0, column =0)
+
 
 def pride():
     print('Pride Mode')
@@ -23,10 +26,10 @@ def result():
     print('Result Mode')
 
 win= Tk()
-home= Frame(win,bg ='white')
+home= Frame(win,bg ='white', bd = 0)
 home.pack()
 win.wm_title('Renaissance Card Sorter')
-welcome = Label(home, text = 'Renaissance Card Sorter', foreground ='dark red', font = 'Arial 16')
+welcome = Label(home, text = 'Renaissance Card Sorter', foreground ='dark red',background ='white', font = 'Arial 16')
 welcome.grid(row = 0 , column = 0)
 gpaButton = Button(home, text = 'GPA Mode', font ='Arial 12', command = gpa)
 gpaButton.grid(row = 1, column = 0)
