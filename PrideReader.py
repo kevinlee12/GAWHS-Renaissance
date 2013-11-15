0b9d08cafab3c1414d7b0de554028ca43a08c0d9
kevinlee963 <kevinlee963@users.noreply.github.com>
2013-11-15 13:44:30 -0800

    Update PrideReader.py
    Fixed student extractor bug

--- PrideReader.py
+++ PrideReader.py
@@ -2,6 +2,7 @@
 #PRIDE Reader
 #-----------------------------------------------------
 def teacher():
+    'Teacher extractor'
     import re
     teacher_pat = re.compile(r'(?P<pat>Teache)'r'(?P<teacher_last>[-a-zA-Z]+, [-a-zA-Z]+)')
     check = []
@@ -11,12 +12,13 @@ def teacher():
             pass
         else:
             print(teacher[0][1])
-
+#'r' (?P<middle_name>[-a-zA-Z]+)?'
 def student():
+    'Student extractor'
     import re
-    student_pat = re.compile(r'(?P<gender>[FM] )'r'(?P<grade>\d\d  )'r'(?P<name>[-a-zA-Z]+, [-a-zA-Z]+)'r' (?P<middle_name>[-a-zA-Z]+)?')
-    check_stu = []
     count = 0
+    student_pat = re.compile(r'(?P<gender>[FM] )'r'(?P<grade>\d\d  )'r'(?P<name>[-a-zA-Z ]+, [-a-zA-Z ]+)')
+    check_stu = []
     for line in file.readlines():
         student = student_pat.findall(line)
         #print(student)
@@ -26,7 +28,7 @@ def student():
             print(student[0])
             count = count + 1
     print(count)
-
+    print('1616 students')
 
 #file_name = input('File Name: ')
 file_name = 'PRIDE'
