2d6c37ea37d031f5b07cb4c22c559d9bb3ad241e
kevinlee963 <kevinlee963@users.noreply.github.com>
2013-12-14 22:05:49 -0800

    Update RenCard_dev.py

--- RenCard_dev.py
+++ RenCard_dev.py
@@ -10,14 +10,63 @@
 #-------------------------------------------------------------------------------
 #Import for all functions needed
 from tkinter import *
+from tkinter.ttk import *
+from tkinter import ttk
 from tkinter import filedialog
 import sys
 import re
 import csv
 import os
+#---------End Import------
+
+def gpa():
+    'GPA Ranker and Counter'
+    #CSV File (out_file in Decider) will now be sheet
+    sheet_filename = filedialog.askopenfilename(filetypes = (("CSV Files", ".csv"),("All files", "*.*")))
+    sheet = open(sheet_filename, 'r')
+    new_sheet = open('GPAOutput.csv','w')
+    writer =csv.writer(new_sheet, dialect='excel')
+    sheet_reader = csv.reader(sheet, dialect="excel")
+    sheet.seek(1)
+    for row in sheet_reader:
+        print(row)
+        print(type(row[4]))
+        something = input(':')
+        if row[4] == 'GPA':
+
+        try:
+            row[4] = float(row[4])
+            print(type(row[4]))
+            if row[4] == 0:
+                row[5] = 'wildcat'
+            elif 0<= row[4] and row[4]<2:
+                row[5] = 'none'
+            elif 2<= row[4] and row[4]<2.5:
+                row[5] = 'bronze'
+            elif 2.5<=row[4] and row[4]<3:
+                row[5] = 'silver'
+            elif 3<=row[4] and row[4]<3.5:
+                row[5] = 'gold'
+            elif 3.5<=row[4] and row[4]<6:
+                row[5] = 'platinum'
+            print(row)
+            writer.writerow(row) #needs fixing
+        except ValueError:
+            pass
+        except:
+            sheet.close()
+            quit
+    try:
+        sheet.close()
+        print('done')
+        exit
+    except ValueError:
+        pass
+        quit
+    #step1.destroy()
 
 def decider():
-    'Pride document reader. Reads PRIDE file and outputs CSV File'
+    '''Pride document reader. Reads PRIDE file and outputs CSV File'''
     prev_teacher = []
     file = open(file_n,'r')
     #output_name = 'Output'
@@ -50,36 +99,37 @@ def decider():
     file.close()
     out_file.close()
     os.system(str("start excel.exe " + output))
+    home.destroy()
+    step1 = ttk.Frame(win)
+    step1.pack()
+    done_step1 = ttk.Button(step1, text = 'Done', command = gpa)
+    done_step1.grid(row = 0, column = 0)
 
 def pride_extractor():
     file_n = filedialog.askopenfilename(filetypes = (("Text Files", ".txt"),("All files", "*.*")))
     global file_n
-    file_name = Label(home,text = file_n ,background = 'white', font = 'Arial 10')
+    file_name = ttk.Label(home,text = file_n)
     file_name.grid(row=1,column = 1)
     if file_n != '':
-        process = Button(home, text = 'Extract!', background = 'green', font = 'Arial 11',command = decider)
+        process = ttk.Button(home, text = 'Extract!',command = decider)
         process.grid(row = 1, column = 2)
 
-def gpa():
-    print('Pride Mode')
-
-def result():
-    print('Result Mode')
-
 win= Tk()
-home= Frame(win,bg ='white', bd = 0)
+win.title('Renaissance Card Sorter')
+top= ttk.Frame(win)
+top.pack()
+home = ttk.Frame(win)
 home.pack()
-win.wm_title('Renaissance Card Sorter')
-welcome = Label(home, text = 'Renaissance Card Sorter', foreground ='dark red',background ='white', font = 'Arial 14')
+style = ttk.Style()
+style.configure("BW.TLabel" ) #Change font size
+welcome = ttk.Label(top,text = 'Renaissance Card Sorter', style = "BW.TLabel")
 welcome.grid(row = 0 , column = 0)
-gpaButton = Button(home, text = 'Pride File', font ='Arial 11', command = pride_extractor)
-gpaButton.grid(row = 1, column = 0)
-#prideButton = Button(home, text ='GPA', font = 'Arial 11', command = gpa)
-#prideButton.grid(row = 2, column = 0)
-#resultButton = Button(home, text= 'Sort & Count', font = 'Arial 11', command = result)
-#resultButton.grid(row = 3, column = 0)
 logo_image = PhotoImage(file = 'logowhite.gif')
-logo = Label(home, image = logo_image)
+logo = ttk.Label(top ,image = logo_image)
 logo.grid(row=0, column =2)
+prideButton = ttk.Button(home, text = 'Pride File', command = pride_extractor)
+prideButton.grid(row = 1, column = 0)
+gpaButton = ttk.Button(home, text = 'GPA Ranker & Counter', command = gpa)
+gpaButton.grid(row = 2, column = 0)
 
 mainloop()
