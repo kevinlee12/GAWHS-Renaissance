1688d38072b9437394bb1d2c2a59e52855b0c187
kevinlee963 <kevinlee963@users.noreply.github.com>
2014-01-11 20:51:46 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+copyDependentFiles=True
+silent = True
+includes = ["tkinter",'tkinter.ttk','sys','re','csv','os','datetime']
+exe=Executable(
+     script="RenCard.py",
+     base="Win32Gui",
+     icon="logo.ico"
+     )
+setup(name='RenCard',
+     version = "1.0",
+     options = {
+       "build_exe" : {
+           "includes": includes,
+           },
+       },
+     executables=[exe],
+ ) 
