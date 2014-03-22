62e7ff00c941736e085912c1ee3c78c956c8393c
kevinlee963 <kevinlee963@users.noreply.github.com>
2014-03-21 17:25:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,13 +26,8 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os",'tkinter','sys','re','csv','datetime'],'include_files':['logowhite.gif']}
+build_exe_options = {"packages": ["os",'tkinter','sys','re','csv','datetime'],'include_files':['logowhite.gif'],'copyDependentFiles':True}
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
 
 exe=Executable(
      script="Rencard.py",
